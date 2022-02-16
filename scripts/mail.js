539c2664dc6a663a53759477a8be7365839f3f81
Stefaan Samyn <stefaan.samyn@hogent.be>
2022-02-16 12:25:40 +0100

    Test site voor publish

--- scripts/mail.js
+++ scripts/mail.js
@@ -1,9 +1,23 @@
+// let sendmail = function () {
+//   alert('Called properly');
+// };
+
 let sendmail = function () {
+  let naamAfzender = document.getElementById('naamAfzender').value;
+  let voornaamAfzender = document.getElementById('voornaamAfzender').value;
+  let emailAfzender = document.getElementById('emailAfzender').value;
+  let bericht = document.getElementById('berichtAfzender').value;
+  let body = `<p>Bericht van: ${voornaamAfzender} ${naamAfzender}</p>
+  <p>E-mail adres: ${emailAfzender}</p>
+  <p>Inhoud:</p>
+  <p>${bericht}</p>
+
+  `;
   Email.send({
     SecureToken: '93e617ac-c146-43c8-85c0-367669df2ca8',
-    To: 'stefaan.samyn@gmail.com',
+    To: 'nyasha.samyn@gmail.com',
     From: 'stefaan.samyn@gmail.com',
     Subject: 'Aanvraag Flexi Medium',
-    Body: 'Dit is de aanvraag'
-  }).then((message) => alert('mail has been sent sucessfully'));
+    Body: body
+  }).then((message) => alert('Bedankt!\nWe beantwoorden je zo snel mogelijk.'));
 };
